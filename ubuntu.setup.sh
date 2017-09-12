815a806488737b7604863958631942371d3906fb
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2017-09-12 17:52:45 +0600

    --no-edit

--- ubuntu.setup.sh
+++ ubuntu.setup.sh
@@ -203,7 +203,7 @@ install_chrome(){
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
     update_repository
-    sudo_command install google-chrome-stable
+    install google-chrome-stable
 }
 
 install_skype(){
